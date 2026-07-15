48f2f5975b40a6c30fb4e0bcdb9ad6566b187ef2
Marius Andrei Dumitru <mariusandrei434@gmail.com>
2026-07-15 14:47:01 +0300

    commit

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'technest_db'); // Numele bazei tale
 define('DB_USER', 'technest_user'); // Utilizatorul creat
-define('DB_PASS', 'parola_ta_puternica'); // Parola
+define('DB_PASS', 'D[JBc?a9AG77U[,p'); // Parola
 
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
